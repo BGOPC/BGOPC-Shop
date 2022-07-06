6bc94f9ad93cdd1daed3f75a94f9fdc54199ff9f
BGOPC <bb8933400@gmail.com>
2022-07-06 17:44:35 +0430

    DB ReBased

--- product/admin.py
+++ product/admin.py
@@ -3,27 +3,7 @@ from .models import *
 
 
 # Register your models here.
-class ProductAdmin(admin.ModelAdmin):
-    readonly_fields = ['rates']
-    prepopulated_fields = {
-        'slug': ['title']
-    }
-    list_display = [
-        "__str__",
-        'slug',
-        'price',
-        'rates',
-        'ia',
-        'category'
-    ]
-    list_filter = [
-        'ia',
-        'price',
-        'title'
-    ]
-    list_editable = [
-        'ia'
-    ]
-admin.site.register(Product, ProductAdmin)
+
+admin.site.register(Product)
 admin.site.register(ProductCategory)
 admin.site.register(ProductTag)
--- product/models.py
+++ product/models.py
@@ -1,26 +1,16 @@
-from itertools import product
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+# from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
 
 # Create your models here.
-
-class ProductTag(models.Model):
-    title = models.CharField(max_length=255, blank=True)
-
-    class Meta:
-        verbose_name = "product tag"
-        verbose_name_plural = "product tags"
-    def __str__(self):
-        return self.title
-
-
 class ProductCategory(models.Model):
-    title = models.CharField(max_length=150, verbose_name="Category Name")
-    url_title = models.CharField(max_length=150, verbose_name="Category Name in url")
-
+    title = models.CharField(max_length=150,db_index=True, verbose_name="Category Name")
+    url_title = models.CharField(max_length=150,db_index=True, verbose_name="Category Name in url")
+    ia = models.BinaryField(verbose_name="Is/ ISn't Active")
+    is_deleted = models.BooleanField(default=False, null=True, verbose_name="is/isn't deleted")
+    
     def __str__(self):
         return f"({self.title}, {self.url_title})"
 
@@ -30,14 +20,14 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=55, unique=True, null=False)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True, related_name="Products")
+    title = models.CharField(max_length=55, null=False)
+    category = models.ManyToManyField (ProductCategory, related_name="Product_Categories")
     price = models.IntegerField(null=False)
+    short_description = models.CharField(max_length=500)
     desc = models.TextField(null=False)
-    rates = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null=True, default=1)
-    ia = models.BooleanField(default=False, null=True)  # is active
-    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
-    product_tags = models.ManyToManyField(ProductTag, blank=True, related_name="product_tags")
+    ia = models.BooleanField(default=False, null=True, verbose_name="is/isn't active")  # is active
+    is_deleted = models.BooleanField(default=False, null=True, verbose_name="is/isn't deleted")
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True, max_length=100, unique=True)
 
     # slug = models.SlugField(default="", null=False, db_index=True, editable=False)
 
@@ -56,3 +46,15 @@ class Product(models.Model):
     ):
         self.slug = slugify(self.title)
         super().save(force_insert=False, force_update=False, using=None, update_fields=None)
+
+class ProductTag(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    product = models.ForeignKey(Product, blank=True, null=True, on_delete=models.SET_NULL, related_name="product_tags" )
+
+    class Meta:
+        verbose_name = "product tag"
+        verbose_name_plural = "product tags"
+    def __str__(self):
+        return self.title
+
+
