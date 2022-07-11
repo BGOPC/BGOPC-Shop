4bcc5f8792b9689c14bf9daedf8ab8e387c82e1c
BGOPC <bb8933400@gmail.com>
2022-07-11 20:40:37 +0430

    Updating Contact and adding authentication

--- contact/static/contact/check.js
+++ contact/static/contact/check.js
@@ -1,7 +1,10 @@
 function check() {
     var form = document.forms["contact-form"];
     Array.from(form.elements).forEach(element => {
-        if (element.value == '' || element.value == null || element.value == undefined || element.value == ' ') return false;
+        if (element.value == '' || element.value == null || element.value == undefined || element.value == ' ') {
+            alert("Please Fill All Fields");
+            return false;
+        };
     });
     return true;
 }
