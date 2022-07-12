c4620ba378c91d2555e27cf20f6979ceed68de2a
BGOPC <bb8933400@gmail.com>
2022-07-12 18:53:22 +0430

    Adding Djnago Heroku for heroku

--- BGOPC_shop/settings.py
+++ BGOPC_shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,4 @@ STATICFILES_DIR = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 COPY_START_YEAR = 2022
+django_heroku.settings(locals())
