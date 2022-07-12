fa04601d3336b7cd3d609a43c519f91634317787
BGOPC <bb8933400@gmail.com>
2022-07-12 19:19:03 +0430

    Adding Secret key loaf

--- BGOPC_shop/settings.py
+++ BGOPC_shop/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import django_heroku
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "qIHDT00dd3cFO6psUDrX2terpRAElcYu9RroEfJtNy0="
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
