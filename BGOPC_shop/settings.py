232004739e05a02f71e632e1416daf71b5733a46
BGOPC <bb8933400@gmail.com>
2022-07-12 18:57:48 +0430

    Edit in Settings

--- BGOPC_shop/settings.py
+++ BGOPC_shop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-plxy)&iffixm4b8fro_atf6!8d2%^i33g_!2o#k0j-_e7*am5_'
+# SECRET_KEY = 'django-insecure-plxy)&iffixm4b8fro_atf6!8d2%^i33g_!2o#k0j-_e7*am5_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
